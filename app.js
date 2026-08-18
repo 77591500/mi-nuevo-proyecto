823c5ba509f20e146c5d5127f990f83da71b2ecd
StevenZH <saturgalaxy12@gmail.com>
2026-08-18 11:42:34 -0600

    primer commit limpio

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>¡Hola Mundo desde mi Pipeline Automático! 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
